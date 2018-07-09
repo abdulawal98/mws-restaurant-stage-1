c836c19c605401ba213462108e010312b8fedbb7
DESKTOP-IM237PM\mdawa <awalcse@gmail.com>
2018-07-08 23:49:18 -0400

    Fix alignment, text size , add tabIndex attributes

--- js/main.js
+++ js/main.js
@@ -151,11 +151,11 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.alt ='';
+  image.alt ='Restaurant image';
   li.append(image);
 
   const name = document.createElement('h1');
-  name.innerHTML = restaurant.name;
+  name.innerHTML = restaurant.name;  
   li.append(name);
 
   const neighborhood = document.createElement('h2');
